43da8a99bca6fae7f8bd8b69ea75312d6d397d34
Ruslan Borisov <rdborisov99@gmail.com>
2022-11-30 13:53:53 +0300

    Final TicTacToe

---
+++ TicTacToe/TicTakToeGame/TicTakToyGame.py
@@ -0,0 +1,298 @@
+import os
+import json
+import random 
+import copy
+
+# Интерфейс игры
+class Game:
+    
+    def __init__(self, field=None):
+        self.field = None
+        if field:
+            self.field = field
+        else:
+            self.start()
+    
+    def start(self):
+        self.field = [' ']*9
+
+    def printField(self):
+        row = ''
+        for i in range(len(self.field)):
+            cell = self.field[i]
+            row+='['
+            if cell != ' ':
+                row+=cell
+            else:
+                row+=str(i+1)
+            row+=']'
+            if (i % 3 == 2): 
+                print(row)
+                row = ''
+    
+    def set(self, position, side):
+        pos = int(position)-1
+        self.field[pos] = side
+
+    def getFree(self):
+        free = []
+        for i in range(len(self.field)):
+            cell = self.field[i]
+            if (cell == ' '):
+                free.append( (i+1) )
+        return free
+    
+    def isDraw(self):
+        free = self.getFree();
+        return len(free)==0;
+
+    def isWin(self, side):
+        for i in range(3):
+            isW = True 
+            for j in range(3):
+                if self.field[i*3+j]!=side:
+                    isW = False
+                    break
+            if isW:
+                return isW   
+        for i in range(3):
+            isW = True 
+            for j in range(3):
+                if self.field[j*3+i]!=side:
+                    isW = False
+                    break
+            if isW:
+                return isW 
+        isW = True;
+        for i in range(3):
+            if self.field[i*3+i]!=side:
+                isW = False
+                break
+        if isW:
+            return isW
+        
+        isWi = True;
+        for i in range(3):
+            if self.field[(i*3+2-i)]!=side:
+                isW = False
+                break
+        if isW:
+            return isW
+        return False
+
+    def getState(self, side):
+        if side == 'x':
+            return self.field
+        newField = ''
+        for i in range(len(self.field)):
+            if self.field[i]=='x':
+                newField += 'o'
+            elif self.field[i]=='o':
+                newField += 'x'
+            else:
+                newField += self.field[i] 
+        return newField
+
+# Описание логики памяти агента и оценка состояний игры
+class AI:
+    def __init__(self):
+        self.table = {}
+        if os.path.isfile('./rewards.json'):
+            with open('rewards.json') as json_file:
+                self.table = json.load(json_file)
+                print("loaded AI from rewards.json")
+
+    def getReward(self, state):
+        game = Game(state)
+        
+        #если победитель - мы, то оценка состояния игры "1"
+        if game.isWin('x'):
+            return 1
+
+        #если победиль - соперник, то оценка состояния игры "0"
+        if game.isWin('o'):
+            return 0
+
+        #смотрим ценность по таблице
+        strstate = ''.join(state)
+        if strstate in self.table.keys():
+            return self.table[strstate]
+
+        #если в таблице нет, то считаем начальной ценностью "0.5"
+        return 0.5
+
+    def correct(self, state, newReward):
+        oldReward = self.getReward(state)
+        strstate = ''.join(state)
+        self.table[strstate] = oldReward + 0.1 * (newReward - oldReward)
+
+    def save(self):
+        with open('rewards.json', 'w') as outfile:
+            json.dump(self.table, outfile)
+
+# Описание агента
+class AIPlayer:
+    
+    def __init__(self, side, ai, isGreedy=True):
+        self.side = side
+        self.ai = ai
+        self.oldState = None
+        self.isGreedy = isGreedy
+
+    def getSide(self):
+        return self.side
+
+    def makeStep(self, game):
+        #получаем список доступных ходов
+        free = game.getFree()
+        
+        #решаем, является ли текущий ход 
+        #зондирующим (случайным) или жадным (максимально выгодным)
+        
+        if not self.isGreedy:
+            #случайный ход
+            print('Random step')
+            step = random.choice(free)
+            game.set(step, self.side)
+            self.oldState = game.getState(self.side)
+            return step
+
+        #жадный ход
+        rewards = {}
+        for step in free:
+            # для каждого доступного хода оцениваем состояние игры после него
+            newGame = copy.deepcopy(game)
+            newGame.set(step, self.side)
+            rewards[step] = self.ai.getReward(newGame.getState(self.side))
+
+        #выясняем, какое вознаграждение оказалось максимальным
+        maxReward = 0
+        for reward in rewards.values():
+            if reward > maxReward:
+                maxReward = reward
+
+        #находим все шаги с максимальным вознаграждением
+        steps = []
+        
+        for step in rewards:
+            reward = rewards[step]
+            if (maxReward > (reward - 0.01)) and (maxReward < (reward + 0.01)):
+                steps.append(step)
+
+        #корректируем оценку прошлого состояния
+        #с учетом ценности нового состояния
+        if (self.oldState):
+            self.ai.correct(self.oldState, maxReward)
+
+        #выбираем ход из ходов с максимальный вознаграждением
+        step = random.choice(steps)
+        game.set(step, self.side)
+
+        #сохраняем текущее состояние для того, 
+        #чтобы откорректировать её ценность на следующем ходе
+        self.oldState = game.getState(self.side)
+        return step
+
+    def loose(self):
+        #корректируем ценность предыдущего состояния при проигрыше
+        if self.oldState:
+            self.ai.correct(self.oldState, 0)
+
+    def win(self):
+        #корректируем ценность предыдущего состояния при выигрыше
+        if self.oldState:
+            self.ai.correct(self.oldState, 1)
+    
+    def draw(self):
+        #корректируем ценность предыдущего состояния при ничьей
+        if self.oldState:
+            self.ai.correct(self.oldState, 0.5)
+# Логика пользовательского ввода
+class UserPlayer:
+
+    def __init__(self, side):
+        self.side = side
+
+    def getSide(self):
+        return self.side
+
+    def makeStep(self, game):
+        game.printField()
+        
+        free = game.getFree()
+
+        inp = None
+        while (True):
+            inp = input()
+            if int(inp) in free:
+                break
+        
+        game.set(inp, self.side)
+
+    def loose(self):
+        print('you loose')
+
+    def win(self):
+        print('you win')
+
+    def draw(self):
+        print('draw')
+
+# Класс запуска
+print('Choose your side:')
+print('X) x')
+print('O) o')
+print('Any other symbol if you would like to run AI vs AI')
+side = input();
+
+ai = AI()
+gameCount = 1
+
+playerX = None
+playerO = None
+
+if (side=='X') or (side=='x'):
+    playerX = UserPlayer('x')
+    playerO = AIPlayer('o', ai, True)
+elif (side=='O') or (side=='o'):
+    playerX = AIPlayer('x', ai, True)
+    playerO = UserPlayer('o')
+else:
+    playerX = AIPlayer('x', ai, True)
+    playerO = AIPlayer('o', ai, True)
+
+    print('Enter games count:')
+    gameCount = int(input())
+
+    if (gameCount <= 0):
+        gameCount = 1
+
+game = Game()
+for i in range(gameCount):
+    print('New game', i+1)
+    game.start()
+    while (True):
+        if game.isDraw():
+            playerX.draw()
+            playerO.draw()
+            break
+
+        playerX.makeStep(game)
+        if game.isWin(playerX.getSide()):
+            playerX.win()
+            playerO.loose()
+            break
+
+        if game.isDraw():
+            playerX.draw()
+            playerO.draw()
+            break
+
+        field = playerO.makeStep(game)
+        if game.isWin(playerO.getSide()):
+            playerO.win()
+            playerX.loose()
+            break
+    game.printField()
+
+ai.save()
---
+++ TicTacToe/Попытки разобраться/TicTakToe_MinMax/Game.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Threading;
+
+namespace TIC_TAC_TOE
+{
+    class Game
+    {
+        static char[] field = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+        static int player = 1;
+        static int choice;
+        static int flag = 0;
+        static void Main(string[] args)
+        {
+            do
+            {
+                Console.Clear();
+                if (player % 2 == 0)
+                {
+                    Console.WriteLine("0 Chance");
+                }
+                else
+                {
+                    Console.WriteLine("X Chance");
+                }
+                Console.WriteLine("\n");
+                PleyField();
+                choice = int.Parse(Console.ReadLine());
+                if (field[choice] != 'X' && field[choice] != 'O')
+                {
+                    if (player % 2 == 0)
+                    {
+                        field[choice] = 'O';
+                        player++;
+                    }
+                    else
+                    {
+                        field[choice] = 'X';
+                        player++;
+                    }
+                }
+                else
+                {
+                    Thread.Sleep(2000);
+                }
+                flag = CheckWin();
+               
+
+            }
+            while (flag != 1 && flag != -1);
+            Console.Clear();
+            PleyField();
+            if (flag == 1)
+            {
+                Console.WriteLine($"{field[choice]} has won", (player % 2) + 1);
+            }
+            else
+            {
+                Console.WriteLine("Draw");
+            }
+            Console.ReadLine();
+        }
+        private static void PleyField()
+        {
+            Console.WriteLine("     |     |      ");
+            Console.WriteLine("  {0}  |  {1}  |  {2}", field[7], field[8], field[9]);
+            Console.WriteLine("_____|_____|_____ ");
+            Console.WriteLine("     |     |      ");
+            Console.WriteLine("  {0}  |  {1}  |  {2}", field[4], field[5], field[6]);
+            Console.WriteLine("_____|_____|_____ ");
+            Console.WriteLine("     |     |      ");
+            Console.WriteLine("  {0}  |  {1}  |  {2}", field[1], field[2], field[3]);
+            Console.WriteLine("     |     |      ");
+        }
+        private static int CheckWin()
+        {
+            if (field[1] == field[2] && field[2] == field[3])
+            {
+                return 1;
+            }
+            else if (field[4] == field[5] && field[5] == field[6])
+            {
+                return 1;
+            }
+            else if (field[6] == field[7] && field[7] == field[8])
+            {
+                return 1;
+            }
+            else if (field[1] == field[4] && field[4] == field[7])
+            {
+                return 1;
+            }
+            else if (field[2] == field[5] && field[5] == field[8])
+            {
+                return 1;
+            }
+            else if (field[3] == field[6] && field[6] == field[9])
+            {
+                return 1;
+            }
+            else if (field[1] == field[5] && field[5] == field[9])
+            {
+                return 1;
+            }
+            else if (field[3] == field[5] && field[5] == field[7])
+            {
+                return 1;
+            }
+            else if (field[1] != '1' && field[2] != '2' && field[3] != '3' && field[4] != '4' && field[5] != '5' && field[6] != '6' && field[7] != '7' && field[8] != '8' && field[9] != '9')
+            {
+                return -1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+
+    class AI
+    { 
+        public static void RandomAI(char[] field)
+        {
+
+        }
+    
+    }
+}
