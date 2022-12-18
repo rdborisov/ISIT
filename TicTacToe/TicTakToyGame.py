7d68a2db3efabcae2f0682b663900a76fd14afd5
Ruslan Borisov <rdborisov99@gmail.com>
2022-12-18 20:48:06 +0300

    Delete trash

--- TicTacToe/TicTakToyGame.py
+++ TicTacToe/TicTakToyGame.py
@@ -1,8 +1,15 @@
+import os
+import json
+import random
+import copy
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Интерфейс игры
 class Game:
 
     def __init__(self, field=None):
         self.field = None
-
         if field:
             self.field = field
         else:
@@ -38,8 +45,8 @@ class Game:
         return free
 
     def isDraw(self):
-        free = self.getFree();
-        return len(free) == 0;
+        free = self.getFree()
+        return len(free) == 0
 
     def isWin(self, side):
         for i in range(3):
@@ -50,7 +57,6 @@ class Game:
                     break
             if isW:
                 return isW
-
         for i in range(3):
             isW = True
             for j in range(3):
@@ -59,7 +65,6 @@ class Game:
                     break
             if isW:
                 return isW
-
         isW = True;
         for i in range(3):
             if self.field[i * 3 + i] != side:
@@ -75,13 +80,11 @@ class Game:
                 break
         if isW:
             return isW
-
         return False
 
     def getState(self, side):
         if side == 'x':
             return self.field
-
         newField = ''
         for i in range(len(self.field)):
             if self.field[i] == 'x':
@@ -90,21 +93,17 @@ class Game:
                 newField += 'x'
             else:
                 newField += self.field[i]
-
         return newField
 
 
-import os
-import json
-
-
+# Описание логики памяти агента и оценка состояний игры
 class AI:
-    def __init__(self, fname):
+    def __init__(self):
         self.table = {}
-        if os.path.isfile(f'./{fname}'):
-            with open(fname) as json_file:
+        if os.path.isfile('./rewards.json'):
+            with open('rewards.json') as json_file:
                 self.table = json.load(json_file)
-                print(f"loaded AI from {fname}")
+                print("loaded AI from rewards.json")
 
     def getReward(self, state):
         game = Game(state)
@@ -131,15 +130,13 @@ class AI:
         self.table[strstate] = oldReward + 0.1 * (newReward - oldReward)
 
     def save(self):
-        with open(fname, 'w') as outfile:
+        with open('rewards.json', 'w') as outfile:
             json.dump(self.table, outfile)
 
 
-import random
-import copy
-
-
+# Описание агента
 class AIPlayer:
+
     def __init__(self, side, ai, isGreedy=True):
         self.side = side
         self.ai = ai
@@ -157,12 +154,12 @@ class AIPlayer:
         # зондирующим (случайным) или жадным (максимально выгодным)
 
         if not self.isGreedy:
-            if ((random.randint(0, 100)) < 30):
-                # случайный ход
-                step = random.choice(free)
-                game.set(step, self.side)
-                self.oldState = game.getState(self.side)
-                return step
+            # случайный ход
+            print('Random step')
+            step = random.choice(free)
+            game.set(step, self.side)
+            self.oldState = game.getState(self.side)
+            return step
 
         # жадный ход
         rewards = {}
@@ -214,3 +211,123 @@ class AIPlayer:
         # корректируем ценность предыдущего состояния при ничьей
         if self.oldState:
             self.ai.correct(self.oldState, 0.5)
+
+
+# Логика пользовательского ввода
+class UserPlayer:
+
+    def __init__(self, side):
+        self.side = side
+
+    def getSide(self):
+        return self.side
+
+    def makeStep(self, game):
+        game.printField()
+
+        free = game.getFree()
+
+        inp = None
+        while (True):
+            inp = input()
+            if int(inp) in free:
+                break
+
+        game.set(inp, self.side)
+
+    def loose(self):
+        print('you loose')
+
+    def win(self):
+        print('you win')
+
+    def draw(self):
+        print('draw')
+
+
+# Класс запуска
+
+plt.figure(figsize=(12, 7))
+
+def graphic():
+    print('x:', countX, 'o:', countO, 'draw:', countD)
+    plt.legend(loc=5)
+    plt.xlabel('Проведено игр')
+    plt.ylabel('Победы')
+    plt.title('График зависимости побед от общего кол-ва игр')
+    plt.legend(['X', 'O', 'D'], loc=2)
+    plt.grid(True)
+    plt.show()
+
+print('Choose your side:')
+print('X) x')
+print('O) o')
+print('Any other symbol if you would like to run AI vs AI')
+side = ' '  # input();
+
+ai = AI()
+gameCount = 1
+
+playerX = AIPlayer('x', ai, True)
+playerO = AIPlayer('o', ai, True)
+
+if (side == 'X') or (side == 'x'):
+    playerX = UserPlayer('x')
+    playerO = AIPlayer('o', ai, True)
+elif (side == 'O') or (side == 'o'):
+    playerX = AIPlayer('x', ai, True)
+    playerO = UserPlayer('o')
+else:
+    playerX = AIPlayer('x', ai, True)
+    playerO = AIPlayer('o', ai, True)
+
+    print('Enter games count:')
+    gameCount = 500000  # int(input())
+
+    if (gameCount <= 0):
+        gameCount = 1
+
+game = Game()
+
+countX = 0
+countO = 0
+countD = 0
+
+for i in range(gameCount):
+    print('New game', i + 1)
+    game.start()
+    while (True):
+        if game.isDraw():
+            playerX.draw()
+            playerO.draw()
+            countD += 1
+            break
+
+        playerX.makeStep(game)
+        if game.isWin(playerX.getSide()):
+            playerX.win()
+            playerO.loose()
+            countX += 1
+            break
+
+        if game.isDraw():
+            playerX.draw()
+            playerO.draw()
+            countD += 1
+            break
+
+        field = playerO.makeStep(game)
+        if game.isWin(playerO.getSide()):
+            playerO.win()
+            playerX.loose()
+            countO += 1
+            break
+
+    game.printField()
+
+    plt.plot(i, countX, 'o-r', alpha=1, lw=2, color='k', mew=1, ms=1)
+    plt.plot(i, countO, 'o-r', alpha=1, lw=2, color='g', mew=1, ms=1)
+    plt.plot(i, countD, 'o-r', alpha=1, lw=2, color='b', mew=1, ms=1)
+
+ai.save()
+graphic()
